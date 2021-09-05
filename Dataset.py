28e51e9a5bb5d5cdc1183f7efae4c59723d6df89
Oscar <s110616038@stu.ntue.edu.tw>
2021-09-05 18:02:45 +0800

    [Add]make getData return the team tiff in 32days

--- Dataset.py
+++ Dataset.py
@@ -7,10 +7,15 @@ from torch.utils.data import Dataset
 
 def getData(mode ='train'):
     if mode =='train':
+        # for training
         # given 1981/1/1~2009/12/31 for training
         # TODO: think about another sampling way
         # cut 32days in 5/1~11/8 each years
+        filenameCombinedResult = list()
+        teamList = list()
+        index = 0
         for files in sorted(os.listdir(r'./data/daily/')):
+
             fileYear = int(files[:4])
             fileMonth = int(files[4:6])
             fileDay = int(files[6:-4])
@@ -22,16 +27,20 @@ def getData(mode ='train'):
             endDayinLastMonth = 8
 
             if lowerYear <= fileYear <= upperYear and lowerMonth <= fileMonth <= upperMonth:
-                # for training
-                # dataset = rasterio.open(r'./data/daily/' + files)
-                if fileMonth==upperMonth and fileDay>endDayinLastMonth:
+                if fileMonth == upperMonth and fileDay > endDayinLastMonth:
                     continue
                 print(f"load Datafile:{files}")
-
+                teamList.append(files)
+                index += 1
+                if index != 0 and index % 32 == 0:
+                    # 32: means the default length of a sequence
+                    filenameCombinedResult.append(teamList)
+                    teamList = list()
 
     else:
         # given 2010/1/1~2019/12/31 for testing
         pass
+    return filenameCombinedResult
 
 class PrecipitationDataset(Dataset):
     def __init__(self):
@@ -48,4 +57,5 @@ class PrecipitationDataset(Dataset):
                 dataset = rasterio.open(r'./data/daily/' + files)
 
 if __name__ == "__main__":
-    getData(mode = "train")
+    combineData = getData(mode = "train")
+    print(combineData)
