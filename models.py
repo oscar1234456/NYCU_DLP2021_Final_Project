45b3bc97bd4b6fda67999afebc75daa24b04e492
Oscar <s110616038@stu.ntue.edu.tw>
2021-09-05 20:06:01 +0800

    [Add]add encoder architecture

--- models.py
+++ models.py
@@ -3,10 +3,18 @@ import torch.nn as nn
 
 class Encoder(nn.Module):
     def __init__(self):
-        pass
-    def forward(self):
-        pass
+        self.convLayer = nn.Sequential(
+            nn.Conv3d(in_channels=32, out_channels=128, kernel_size=(3,3,3), stride=(2,2,2)),
+            nn.Conv3d(in_channels=128, out_channels=128, kernel_size=(3,3,3), stride=(2,2,2)),
+            nn.Linear(128, 500),
+            nn.ReLU()
+        )
+        self.linear_mean = nn.Linear(500, 30)
+        self.linear_logVar = nn.Linear(500, 30)
 
+    def forward(self, input):
+        # input: N*32*32*32
+        pass
 
 class Decoder(nn.Module):
     def __init__(self):
