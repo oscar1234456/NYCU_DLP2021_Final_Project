a9fb0c90cf563635fe8aa6a11d452a0c767461b8
Oscar <s110616038@stu.ntue.edu.tw>
2021-09-05 20:40:33 +0800

    [Add&Fix]Fix encoder architecture

--- models.py
+++ models.py
@@ -3,10 +3,13 @@ import torch.nn as nn
 
 class Encoder(nn.Module):
     def __init__(self):
+        super(Encoder, self).__init__()
         self.convLayer = nn.Sequential(
-            nn.Conv3d(in_channels=32, out_channels=128, kernel_size=(3,3,3), stride=(2,2,2)),
+            nn.Conv3d(in_channels=1, out_channels=128, kernel_size=(3,3,3), stride=(2,2,2)),
             nn.Conv3d(in_channels=128, out_channels=128, kernel_size=(3,3,3), stride=(2,2,2)),
-            nn.Linear(128, 500),
+        )
+        self.linearLayer = nn.Sequential(
+            nn.Linear(in_features=43904, out_features=500),
             nn.ReLU()
         )
         self.linear_mean = nn.Linear(500, 30)
@@ -14,12 +17,25 @@ class Encoder(nn.Module):
 
     def forward(self, input):
         # input: N*32*32*32
-        out = self.convLayer(input)
-        pass
+        out = self.convLayer(input.view(-1, 1, 32, 32, 32)) #out: N*128*
+        out = self.linearLayer(out.view(-1,128*7*7*7))
+        linearMean = self.linear_mean(out)
+        linearLogVar = self.linear_logVar(out)
+        return linearMean, linearLogVar
 
 class Decoder(nn.Module):
     def __init__(self):
+        super(Decoder, self).__init__()
         pass
 
     def forward(self):
         pass
+
+
+
+if __name__ == "__main__":
+    testTensor = torch.randn(5,32,32,32)
+    encoder = Encoder()
+    mean, log_var = encoder(testTensor)
+    print(mean)
+    print(log_var)
